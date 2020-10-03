6c2cc334c19856bb489e3c27bd6b8f7c6b33d534
Kingsley Uchunor <kizucom@gmail.com>
2020-10-03 19:16:54 +0100

    app update

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+  private _productService: ProductService;
+  albumInfo;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
 
+    }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 
-import { Http Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import rxjs/add/operator/map;
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductService {
-  private _http;
-  constructor(Http)
+  private _http: Http;
+  private _albumUrl: string;
+
+  constructor(){
+    this._albumUrl = '../assets/album.json';
+  }
+
+  function getAlbum(id:number)
   {
-    _http : Http
+    result  = this._http.get(_albumUrl);
+    return result;
   }
 }
